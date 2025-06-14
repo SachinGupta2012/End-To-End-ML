83f863c2573a9aca4552f7c1bf3647ac34de2d77
SachinGupta2012 <sachinguptadec03@gmail.com>
2025-06-14 05:24:04 +0000

    Seventh

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -47,4 +48,6 @@ if __name__ == "__main__":
     print("Data ingestion completed successfully.")
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr, preprocessor_path=data_transformation.data_transformation_config.preprocessor_obj_file_path))
